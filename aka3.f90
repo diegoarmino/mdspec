b39853a2cc73284d509533dc669c31affe0ff5d9
diegoarmino <diegoarmino@gmail.com>
2020-04-03 07:06:48 -0300

    Added full pump-probe functionality: do difference spectra between excited and unexcited systems and also added computation of kinetic energies.

--- aka3.f90
+++ aka3.f90
@@ -1,6 +1,6 @@
 program aka2
 implicit none
-character(len=50) :: infile,topfile
+character(len=50) :: eqmd_file,neq1_file,neq2_file,topfile
 integer :: nsteps, spatial, natoms
 double precision :: coordinate, dt
 double precision, allocatable, dimension (:,:) :: tdspec
@@ -9,23 +9,33 @@ integer                :: specframes
 integer                :: specdim
 INTEGER*8              :: plan
 
-call get_command_argument(1,infile)
+call get_command_argument(1,eqmd_file)
+call get_command_argument(1,neq1_file)
+call get_command_argument(1,neq2_file)
 call get_command_argument(2,topfile)
-call getdims(infile,nsteps,spatial,natoms)
 
+!GET NETCDF INFORMATION
+call getdims(eqmd_file,nsteps,spatial,natoms)
+dt=1.0d-15
 specframes=int(nsteps/2)
 specdim=int(specframes/2+1)
 
 !allocate( tdspec((NSTEPS/2+1)/2+1,NSTEPS/2), atmass(natoms) )
 allocate( tdspec(5000,5000), atmass(natoms) )
 
+!GET ATOMIC MASSES FROM PRMTOP FILE AND SQRT THEM.
 call get_at_mass_prmtop(natoms,topfile,atmass)
 
-dt=1.0d-15
-call make_fftw_plan(infile,nsteps,plan)
-call td_spectrum(infile,atmass,plan,nsteps,natoms,spatial,dt,tdspec)
+!MAKE PLAN FOR FOURIER TRANSFORM LIBRARY
+call make_fftw_plan(eqmd_file,nsteps,plan)
+
+!COMPUTE PUMP-PROBE SPECTRA
+call td_spectrum(eqmd_file,neq1_file,neq2_file,atmass,plan,nsteps,natoms,spatial,dt,tdspec)
 call print_results(nsteps,dt,tdspec)
-   
+
+! COMPUTE KINETIC ENERGY DIFFERENCES.
+call kinetic_energy(eqmd_file,neq1_file,neq2_file,atmass,plan,nsteps,natoms,spatial,dt)
+
 !Destroying fftw plan
 call dfftw_destroy_plan(plan)
 
@@ -118,23 +128,23 @@ write(*,*) 'FINISHED FFTW PLAN!'
 
 end subroutine make_fftw_plan
 
-SUBROUTINE td_spectrum(infile,atmass,plan,nsteps,natoms,spatial,dt,tdspec)
+SUBROUTINE td_spectrum(eqmd_file,neq1_file,neq2_file,atmass,plan,nsteps,natoms,spatial,dt,tdspec)
 USE netcdf
 USE,INTRINSIC :: iso_c_binding
 IMPLICIT NONE
 include 'fftw3.f03'
-CHARACTER(LEN=50), INTENT(IN) :: infile
+CHARACTER(LEN=50), INTENT(IN) :: eqmd_file,neq1_file,neq2_file
 INTEGER(KIND=4),   INTENT(IN) :: natoms, nsteps, spatial
 INTEGER*8,         INTENT(IN) :: plan
 DOUBLE PRECISION,  INTENT(IN) :: atmass(natoms)
 DOUBLE PRECISION :: coordinate,dt
-DOUBLE PRECISION, DIMENSION(NSTEPS) :: coordinate_evol
+DOUBLE PRECISION, DIMENSION(NSTEPS) :: eqmd_traj,neq1_traj,neq2_traj
 DOUBLE PRECISION, DIMENSION(NSTEPS/2) :: veltraj_window
 COMPLEX*16      , DIMENSION((NSTEPS/2+1)/2+1) :: out
 !DOUBLE PRECISION, DIMENSION((NSTEPS/2+1)/2+1,NSTEPS/2),intent(out) :: tdspec
 DOUBLE PRECISION, DIMENSION(5000,5000),intent(out) :: tdspec
 
-INTEGER(KIND=4) :: ncid, xtype, ndims, varid
+INTEGER(KIND=4) :: ncid1, ncid2, ncid3, xtype, ndims, varid
 CHARACTER(LEN=50) :: xname, vname
 CHARACTER(LEN=20) :: fmt
 integer, dimension(3) :: point,endp
@@ -143,29 +153,135 @@ integer :: i,j,k,time0,ierr,specdim,specframes
 specframes=int(nsteps/2)
 specdim=int(specframes/2+1)
 
-CALL check(nf90_open(infile, nf90_nowrite, ncid))
+CALL check(nf90_open(eqmd_file, nf90_nowrite, ncid1))
+CALL check(nf90_open(neq1_file, nf90_nowrite, ncid2))
+CALL check(nf90_open(neq2_file, nf90_nowrite, ncid3))
 
 tdspec=0d0
-veltraj_window=0d0
+eqmd_traj=0d0
+neq1_traj=0d0
+neq2_traj=0d0
 
 do i = 1,natoms !natoms
   write(*,*) 'STARTING ATOM ',i,' OUT OF ',natoms
   do j = 1,3 !3
       point = (/ j, i, 1 /)
       endp = (/ 1,1,nsteps-1 /)
-      CALL check(nf90_get_var(ncid,4,coordinate_evol,start = point,count = endp))
+      CALL check(nf90_get_var(ncid1,4,eqmd_traj,start = point,count = endp))
+      CALL check(nf90_get_var(ncid2,4,neq1_traj,start = point,count = endp))
+      CALL check(nf90_get_var(ncid3,4,neq2_traj,start = point,count = endp))
       do time0=1,5000
-         veltraj_window=coordinate_evol(time0:time0+nsteps/2-1)
+         veltraj_window=eqmd_traj(time0:time0+nsteps/2-1)
          call dfftw_execute_dft_r2c(plan, veltraj_window, out)
          out = conjg(out)*out
-         tdspec(:,time0) = tdspec(:,time0) + REAL(out(1:5000)) ! DEBUG
+         tdspec(:,time0) = tdspec(:,time0) -       REAL(out(1:5000)) ! DEBUG
+
+         veltraj_window=neq1_traj(time0:time0+nsteps/2-1)
+         call dfftw_execute_dft_r2c(plan, veltraj_window, out)
+         out = conjg(out)*out
+         tdspec(:,time0) = tdspec(:,time0) + 0.5d0*REAL(out(1:5000)) ! DEBUG
+
+         veltraj_window=neq2_traj(time0:time0+nsteps/2-1)
+         call dfftw_execute_dft_r2c(plan, veltraj_window, out)
+         out = conjg(out)*out
+         tdspec(:,time0) = tdspec(:,time0) + 0.5d0*REAL(out(1:5000)) ! DEBUG
       end do
   end do
 end do
-close(1)
-CALL check(nf90_close(ncid))
+
+! CLOSE NETCDF FILES
+CALL check(nf90_close(ncid1))
+CALL check(nf90_close(ncid2))
+CALL check(nf90_close(ncid3))
 
 end subroutine td_spectrum
+
+SUBROUTINE kinetic_energy(eqmd_file,neq1_file,neq2_file,atmass,plan,nsteps,natoms,spatial,dt)
+USE netcdf
+USE,INTRINSIC :: iso_c_binding
+IMPLICIT NONE
+include 'fftw3.f03'
+CHARACTER(LEN=50), INTENT(IN) :: eqmd_file,neq1_file,neq2_file
+INTEGER(KIND=4),   INTENT(IN) :: natoms, nsteps, spatial
+INTEGER*8,         INTENT(IN) :: plan
+DOUBLE PRECISION,  INTENT(IN) :: atmass(natoms)
+DOUBLE PRECISION :: coordinate,dt
+DOUBLE PRECISION, DIMENSION(NSTEPS) :: eqmd_traj,neq1_traj,neq2_traj
+DOUBLE PRECISION, DIMENSION(NSTEPS) :: eqmd_ke,neq1_ke,neq2_ke,dif_ke
+DOUBLE PRECISION, DIMENSION(NSTEPS/2) :: veltraj_window
+COMPLEX*16      , DIMENSION((NSTEPS/2+1)/2+1) :: out
+
+INTEGER(KIND=4) :: ncid1, ncid2, ncid3
+CHARACTER(LEN=50) :: xname, vname
+CHARACTER(LEN=20) :: fmt
+integer, dimension(3) :: point,endp
+integer :: i,j,k,time0,err,specdim,specframes
+
+specframes=int(nsteps/2)
+specdim=int(specframes/2+1)
+
+CALL check(nf90_open(eqmd_file, nf90_nowrite, ncid1))
+CALL check(nf90_open(neq1_file, nf90_nowrite, ncid2))
+CALL check(nf90_open(neq2_file, nf90_nowrite, ncid3))
+
+tdspec    = 0d0
+eqmd_traj = 0d0
+neq1_traj = 0d0
+neq2_traj = 0d0
+eqmd_ke   = 0d0
+neq1_ke   = 0d0
+neq2_ke   = 0d0
+
+do i = 1,natoms !natoms
+  write(*,*) 'STARTING ATOM ',i,' OUT OF ',natoms
+  do j = 1,3 !3
+      point = (/ j, i, 1 /)
+      endp = (/ 1,1,nsteps-1 /)
+      CALL check(nf90_get_var(ncid1,4,eqmd_traj,start = point,count = endp))
+      CALL check(nf90_get_var(ncid2,4,neq1_traj,start = point,count = endp))
+      CALL check(nf90_get_var(ncid3,4,neq2_traj,start = point,count = endp))
+      eqmd_ke = eqmd_ke + 0.5d0 * eqmd_traj * eqmd_traj
+      neq1_ke = neq1_ke + 0.5d0 * neq1_traj * neq1_traj
+      neq2_ke = neq2_ke + 0.5d0 * neq2_traj * neq2_traj
+  end do
+end do
+
+! COMPUTE KINETIC ENERGY DIFFERENCE
+dif_ke = 0.5d0 * (neq1_ke + neq2_ke) - eqmd_ke
+
+! CLOSE NETCDF FILES
+CALL check(nf90_close(ncid1))
+CALL check(nf90_close(ncid2))
+CALL check(nf90_close(ncid3))
+
+!====================================================================
+! PRINT RESULTS
+!====================================================================
+open(unit=123,file='diff_ke.dat',iostat=err)
+do i=1,nsteps-1
+   write(123,*) dif_ke(i)
+end do
+close(123)
+
+open(unit=124,file='eqmd_ke.dat',iostat=err)
+do i=1,nsteps-1
+   write(124,*) eqmd_ke(i)
+end do
+close(124)
+
+open(unit=124,file='neq1_ke.dat',iostat=err)
+do i=1,nsteps-1
+   write(124,*) neq1_ke(i)
+end do
+close(124)
+
+open(unit=124,file='neq2_ke.dat',iostat=err)
+do i=1,nsteps-1
+   write(124,*) neq2_ke(i)
+end do
+close(124)
+
+end subroutine kinetic_energy
 
 subroutine print_results(nsteps,dt,tdspec)
 implicit none
